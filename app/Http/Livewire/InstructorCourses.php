5f7be534c8792d40fbba23fa55d5bafe1674aaf0
Santiago Fandiño <santiago.fango@gmail.com>
2021-11-18 01:25:44 -0500

    Inicio visual de panel de instructores

--- app/Http/Livewire/InstructorCourses.php
+++ app/Http/Livewire/InstructorCourses.php
@@ -4,13 +4,27 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 
+use App\Models\course;
+use Livewire\WithPagination;
+
 class InstructorCourses extends Component
 {
 
+    use WithPagination;
 
+    public $search;
 
     public function render()
     {
-        return view('livewire.instructor-courses');
+
+        $courses = course::where('title', 'LIKE', '%' . $this->search . '%')
+                            ->where('user_id', auth()->user()->id)
+                            ->paginate(8);
+
+        return view('livewire.instructor-courses', compact('courses'));
+    }
+
+    public function limpiar_page(){
+        $this->reset('page');
     }
 }
