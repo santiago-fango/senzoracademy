498c7b5647f1fa12b10bfee6493ecdf947c32bf8
Santiago Fandiño <santiago.fango@gmail.com>
2022-07-16 18:52:56 -0500

    edit welcome prueba

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -51,7 +51,11 @@
                     <div class="lg:text-center">
                         <h2 class="text-base text-red-600 font-semibold tracking-wide uppercase">Sobre nosotros</h2>
                         <p class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">Capacítate con nosotros</p>
-                        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">En Sensor Academy podrás encontrar diversos cursos para tus diversos intereses</p>
+                        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">En </p>
+                        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto font-extrabold">Senzor </p>
+                        <p class="mt-4 max-w-2xl text-xl text-gray-500 lg:mx-auto">Academy podrás encontrar diversos cursos para tus diversos intereses</p>
+
+
                     </div>
                 
                     <div class="mt-10">
