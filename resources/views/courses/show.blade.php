55e203c6affadd88b78858bda95a0dcf7c52e9b7
Santiago Fandiño <santiago.fango@gmail.com>
2022-07-27 18:46:58 -0500

    boton whats - v6 final?

--- resources/views/courses/show.blade.php
+++ resources/views/courses/show.blade.php
@@ -82,7 +82,7 @@
             </section>
 
             <!-- Boton de Whatsapp -->
-            <section class="lg: mx-36">
+            <section class="lg: mx-36 sm: m-0">
                 <div class="flex flex-col px-8 mx-8 bg-green-600 shadow-md rounded-2xl">
                     <a href="https://wa.link/6i4lmo" target="_blank">
                         <div class="flex items-center justify-between">
