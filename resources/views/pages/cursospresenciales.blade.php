c1d5e3a8793b5548381dcb5cbcdb1c10efa0d5b0
Santiago Fandiño <santiago.fango@gmail.com>
2022-07-31 01:54:53 -0500

    New elements pag info

--- resources/views/pages/cursospresenciales.blade.php
+++ resources/views/pages/cursospresenciales.blade.php
@@ -18,7 +18,94 @@
         </div>
     </section>
 
-    
+    <section class="relative  bg-blueGray-50">
+        <div class="relative pt-16 pb-32 flex content-center items-center justify-center min-h-screen-75">
+                <div class="absolute top-0 w-full h-full bg-center bg-cover" style="
+                    background-image: url('https://images.unsplash.com/photo-1557804506-669a67965ba0?ixlib=rb-1.2.1&amp;ixid=eyJhcHBfaWQiOjEyMDd9&amp;auto=format&amp;fit=crop&amp;w=1267&amp;q=80');">
+                  <span id="blackOverlay" class="w-full h-full absolute opacity-75 bg-black"></span>
+                </div>
+                <div class="container relative mx-auto">
+                  <div class="items-center flex flex-wrap">
+                    <div class="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+                      <div class="pr-12">
+                        <h1 class="text-white font-semibold text-5xl">
+                          Capacitate con nosotros
+                        </h1>
+                        <p class="mt-4 text-lg text-blueGray-200">
+                          This is a simple example of a Landing Page you can build using
+                          Notus JS. It features multiple CSS components based on the
+                          Tailwind CSS design system.
+                        </p>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div class="top-auto bottom-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden h-70-px" style="transform: translateZ(0px)">
+                  <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
+                    <polygon class="text-blueGray-200 fill-current" points="2560 0 2560 100 0 100"></polygon>
+                  </svg>
+                </div>
+              </div>
+              <section class="pb-10 bg-blueGray-200 -mt-24">
+                <div class="container mx-auto px-4">
+                  <div class="flex flex-wrap">
+                    <div class="lg:pt-12 pt-6 w-full md:w-4/12 px-4 text-center">
+                      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                        <div class="px-4 py-5 flex-auto">
+                          <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
+                            <i class="fas fa-award"></i>
+                          </div>
+                          <h6 class="text-xl font-semibold">Tipos de inyección</h6>
+                          <p class="mt-2 mb-4 text-blueGray-500">
+                            Divide details about your product or agency work into parts.
+                            A paragraph describing a feature will be enough.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="w-full md:w-4/12 px-4 text-center">
+                      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                        <div class="px-4 py-5 flex-auto">
+                          <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-lightBlue-400">
+                            <i class="fas fa-retweet"></i>
+                          </div>
+                          <h6 class="text-xl font-semibold">Free Revisions</h6>
+                          <p class="mt-2 mb-4 text-blueGray-500">
+                            Keep you user engaged by providing meaningful information.
+                            Remember that by this time, the user is curious.
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="pt-6 w-full md:w-4/12 px-4 text-center">
+                      <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
+                        <div class="px-4 py-5 flex-auto">
+                          <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-emerald-400">
+                            <i class="fas fa-fingerprint"></i>
+                          </div>
+                          <h6 class="text-xl font-semibold">Verified Company</h6>
+                          <p class="mt-2 mb-4 text-blueGray-500">
+                            Write a few lines about each one. A paragraph describing a
+                            feature will be enough. Keep you user engaged!
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                   <footer class="relative  pt-8 pb-6 mt-1">
+          <div class="container mx-auto px-4">
+            <div class="flex flex-wrap items-center md:justify-between justify-center">
+              <div class="w-full md:w-6/12 px-4 mx-auto text-center">
+                <div class="text-sm text-blueGray-500 font-semibold py-1">
+                  Made with <a href="https://www.creative-tim.com/product/notus-js" class="text-blueGray-500 hover:text-gray-800" target="_blank">Notus JS</a> by <a href="https://www.creative-tim.com" class="text-blueGray-500 hover:text-blueGray-800" target="_blank"> Creative Tim</a>.
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
+              </section>
+        </section>
+
       <section class="bg-white dark:bg-gray-900">
         <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
             <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
@@ -38,16 +125,8 @@
                         <li>SENSORES DE OXIGENO</li>
                         <li>ESCANER</li>
                         <li>INYECTORES</li>
-                        <li>EMISIONES Y CATALIZADORES</li>
-                        <li>SISTEMAS DE INYECCION SIN BATERIA</li>
-                        <li>SISTEMAS DE FRENOS ABS</li>
-                        <li>SINCRONIZACION</li>
-                        <li>SISTEMA DE INYECCION YAMAHA</li>
-                        <li>SISTEMA DE INYECCION SUZUKI</li>
-                        <li>SISTEMA DE INYECCION HONDA</li>
-                        <li>SISTEMA DE INYECCION KAWASAKI</li>
-                        <li>SISTEMA DE INYECCION BAJAJ</li>
                     </ul>
+                    <p class="mb-4">Y muchos mas</p>
                 </div>
                 
             </div>
