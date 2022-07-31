291a4f13e6c00b005e3f9c600823fdf6816c7503
Santiago Fandiño <santiago.fango@gmail.com>
2022-07-30 23:19:34 -0500

    Date error - footer

--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -48,7 +48,7 @@
         </div>
         <hr class="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
         <div class="sm:flex sm:items-center sm:justify-between">
-            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© <?phpecho date("y")?> <a href="https://senzor.co" class="hover:underline">Senzor</a>. todos los derechos reservados.
+            <span class="text-sm text-gray-500 sm:text-center dark:text-gray-400">© 2017 - <?phpecho date("Y");?> <a href="https://senzor.co" class="hover:underline">Senzor</a>. todos los derechos reservados.
             </span>
             <div class="flex mt-4 space-x-6 sm:justify-center sm:mt-0">
                 <!-- Facebook Button -->
