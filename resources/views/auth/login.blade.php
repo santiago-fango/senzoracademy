e1f1b1050e0c24ee691e0b82688dea9a37d837b4
Santiago Fandiño <santiago.fango@gmail.com>
2022-08-05 19:21:37 -0500

    Login casi funcional

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,5 +1,6 @@
 <x-guest-layout>
-    <x-jet-authentication-card>
+    <!--
+        <x-jet-authentication-card>
         <x-slot name="logo">
             <x-jet-authentication-card-logo />
         </x-slot>
@@ -45,13 +46,14 @@
             </div>
         </form>
     </x-jet-authentication-card>
+    -->
     
     <div class="bg-white dark:bg-gray-900">
         <div class="flex justify-center h-screen">
             <div class="hidden bg-cover lg:block lg:w-2/3" style="background-image: url(https://images.unsplash.com/photo-1616763355603-9755a640a287?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1470&q=80)">
                 <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
                     <div>
-                        <h2 class="text-4xl font-bold text-white">Brand</h2>
+                        <h2 class="text-4xl font-bold text-white">Senzor</h2>
                         
                         <p class="max-w-xl mt-3 text-gray-300">Lorem ipsum dolor sit, amet consectetur adipisicing elit. In autem ipsa, nulla laboriosam dolores, repellendus perferendis libero suscipit nam temporibus molestiae</p>
                     </div>
@@ -60,10 +62,15 @@
             
             <div class="flex items-center w-full max-w-md px-6 mx-auto lg:w-2/6">
                 <div class="flex-1">
+
+                    <x-slot name="logo">
+                        <x-jet-authentication-card-logo />
+                    </x-slot>
+
                     <div class="text-center">
-                        <h2 class="text-4xl font-bold text-center text-gray-700 dark:text-white">Brand</h2>
+                        <h2 class="text-4xl font-bold text-center text-gray-700 dark:text-white">Senzor</h2>
                         
-                        <p class="mt-3 text-gray-500 dark:text-gray-300">Sign in to access your account</p>
+                        <p class="mt-3 text-gray-500 dark:text-gray-300">Inicia sesíon para acceder a tu cuenta!</p>
                     </div>
 
                     <div class="mt-8">
@@ -71,7 +78,7 @@
                             @csrf
                             <div>
                                 <label for="email" value="{{ __('Email') }}" class="block mb-2 text-sm text-gray-600 dark:text-gray-200">Correo Electronico</label>
-                                <input type="email" name="email" id="email" placeholder="example@example.com" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" :value="old('email')" required autofocus />
+                                <input type="email" name="email" id="email" placeholder="ejemplo@ejemplo.com" class="block w-full px-4 py-2 mt-2 text-gray-700 placeholder-gray-400 bg-white border border-gray-200 rounded-md dark:placeholder-gray-600 dark:bg-gray-900 dark:text-gray-300 dark:border-gray-700 focus:border-blue-400 dark:focus:border-blue-400 focus:ring-blue-400 focus:outline-none focus:ring focus:ring-opacity-40" :value="old('email')" required autofocus />
                             </div>
 
                             <div class="mt-6">
@@ -94,7 +101,7 @@
 
                         </form>
 
-                        <p class="mt-6 text-sm text-center text-gray-400">Don&#x27;t have an account yet? <a href="#" class="text-blue-500 focus:outline-none focus:underline hover:underline">Sign up</a>.</p>
+                        <p class="mt-6 text-sm text-center text-gray-400">Aun no tienes una cuenta? <a href="{{route ('register')}}" class="text-blue-500 focus:outline-none focus:underline hover:underline">Registrate ahora!</a>.</p>
                     </div>
                 </div>
             </div>
