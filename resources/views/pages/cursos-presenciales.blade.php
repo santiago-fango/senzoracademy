b0ada20c433f548599d1ccdd6484689ca19b1a54
Santiago Fandiño <santiago.fango@gmail.com>
2022-08-26 18:09:48 -0500

    edit botton y margenes

--- resources/views/pages/cursos-presenciales.blade.php
+++ resources/views/pages/cursos-presenciales.blade.php
@@ -1,7 +1,7 @@
 <x-app-layout>
 
 <body>
-    <div class="grid gap-12 lg:grid-cols-2 py-4">
+    <div class="grid gap-12 lg:grid-cols-2 py-10 px-10 sm:px-4 py-4">
         <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
           <img src="https://cdn.pixabay.com/photo/2014/07/31/23/37/motorbike-407186_960_720.jpg" alt="inyeccionelectronica" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
           <div class="sm:w-7/12 pl-0 p-5">
@@ -10,7 +10,7 @@
                 <h4 class="text-2xl font-semibold text-black">Curso de Inyección Electrónica</h4>
                 <p class="text-gray-600">Curso especializado en inyección electrónica multimarca (Yamaha, Suzuki, Honda, Kawasaki, Bajaj - Pulsar - Dominar, entre otros) Totalmente presencial.</p>
               </div>
-              <a href="{{route('inyeccion-electronica')}}" class="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-black md:py-4 md:text-lg md:px-10">Quiero Saber Mas!</a>
+              <a href="{{route('inyeccion-electronica')}}" class="w-full flex items-center justify-center px-2 py-2 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-black md:py-4 md:text-lg md:px-4">Quiero Saber Mas!</a>
             </div>
           </div>
         </div>
