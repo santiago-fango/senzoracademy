ebdf14d9f571085fe5a162f4841ac475af609c07
Santiago Fandiño <santiago.fango@gmail.com>
2022-08-26 19:33:31 -0500

    correccion margen item

--- resources/views/pages/inyeccion-electronica.blade.php
+++ resources/views/pages/inyeccion-electronica.blade.php
@@ -46,7 +46,7 @@
         <section class="pb-10 bg-blueGray-200 -mt-24">
                 <div class="container mx-auto px-4">
                   <div class="flex flex-wrap">
-                    <div class="lg:pt-12 w-full md:w-4/12 px-4 text-center">
+                    <div class="w-full md:w-4/12 px-4 text-center">
                       <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
                         <div class="px-4 py-5 flex-auto">
                           <div class="text-white p-3 text-center inline-flex items-center justify-center w-12 h-12 mb-5 shadow-lg rounded-full bg-red-400">
