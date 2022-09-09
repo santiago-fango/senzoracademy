4811269dfdcd56ac093240ee0f1078d18fddfe64
Santiago Fandiño <santiago.fango@gmail.com>
2022-09-09 18:05:48 -0500

    imagen

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -50,7 +50,7 @@
     
     <div class="bg-white dark:bg-gray-900">
         <div class="flex justify-center h-screen">
-            <div class="hidden bg-cover lg:block lg:w-2/3" style="background-image: url({{asset('img/admin/inicio de sesion - registro.jpg')}})">
+            <div class="hidden bg-cover lg:block lg:w-2/3" style="background-image: url({{asset('img/admin/inicio-sesion.png')}})">
                 <div class="flex items-center h-full px-20 bg-gray-900 bg-opacity-40">
                     <div>
                         <h2 class="text-4xl font-bold text-white">Senzor</h2>
