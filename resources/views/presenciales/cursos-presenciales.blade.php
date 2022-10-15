b72146179c96e4d821f5c9cea64e78f3ef6d694f
Santiago Fandiño <santiago.fango@gmail.com>
2022-10-14 19:42:06 -0500

    Corrreccion de distribucion

--- resources/views/presenciales/cursos-presenciales.blade.php
+++ resources/views/presenciales/cursos-presenciales.blade.php
@@ -59,22 +59,23 @@
             </div>
           </div>
         </div>
-    </div>
 
-    <div>
-      <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
-        <img src="{{asset('img/presenciales/portada-mecanica-de-alto-cilindraje-presencial.png')}}" alt="Vehiculos Electricos" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
-        <div class="sm:w-7/12 pl-0 p-5">
-          <div class="space-y-2">
-            <div class="space-y-4">
-              <h4 class="text-2xl font-semibold text-cyan-900">Vehículos Eléctricos (Motocicleta, Bicicleta y Patinetas)</h4>
-              <p class="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quis, quibusdam eligendi hic quaerat maxime nulla consequuntur ut dignissimos, incidunt aut exercitationem consectetur, natus eos iure omnis fugit corporis saepe!</p>
+        <div>
+          <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
+            <img src="{{asset('img/presenciales/portada-mecanica-de-alto-cilindraje-presencial.png')}}" alt="Vehiculos Electricos" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
+            <div class="sm:w-7/12 pl-0 p-5">
+              <div class="space-y-2">
+                <div class="space-y-4">
+                  <h4 class="text-2xl font-semibold text-cyan-900">Vehículos Eléctricos (Motocicleta, Bicicleta y Patinetas)</h4>
+                  <p class="text-gray-600">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum quis, quibusdam eligendi hic quaerat maxime nulla consequuntur ut dignissimos, incidunt aut exercitationem consectetur, natus eos iure omnis fugit corporis saepe!</p>
+                </div>
+                <a href="{{route('vehiculos-electricos')}}" class="w-full flex items-center justify-center p-1 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-black">Quiero Saber Mas!</a>
+              </div>
             </div>
-            <a href="{{route('vehiculos-electricos')}}" class="w-full flex items-center justify-center p-1 border border-transparent text-base font-medium rounded-md text-white bg-red-600 hover:bg-black">Quiero Saber Mas!</a>
           </div>
         </div>
+        
       </div>
-    </div>
 </div>
 </body>
 </x-app-layout>
