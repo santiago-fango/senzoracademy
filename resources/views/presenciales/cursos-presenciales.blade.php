67059997ab8e9e7a200763b5a9c60231a73d02dd
Santiago Fandiño <santiago.fango@gmail.com>
2022-10-27 15:51:44 -0500

    Agg new img in em and center img

--- resources/views/presenciales/cursos-presenciales.blade.php
+++ resources/views/presenciales/cursos-presenciales.blade.php
@@ -3,7 +3,7 @@
 <body>
     <div class="grid gap-12 lg:grid-cols-2 lg:p-6 p-2">
         <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
-          <img src="{{asset('img/presenciales/portada-inyeccion-electronica-presencial.png')}}" alt="inyeccion electronica" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
+          <img src="{{asset('img/presenciales/portada-inyeccion-electronica-presencial.png')}}" alt="inyeccion electronica" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-center rounded-lg transition duration-500 group-hover:rounded-xl">
           <div class="sm:w-7/12 pl-0 p-5">
             <div class="space-y-2">
               <div class="space-y-4">
@@ -17,7 +17,7 @@
 
         <div>
           <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
-            <img src="{{asset('img/presenciales/portada-electricidad-en-motocicletas-presencial.png')}}" alt="Electricidad Basica" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
+            <img src="{{asset('img/presenciales/portada-electricidad-en-motocicletas-presencial.png')}}" alt="Electricidad Basica" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-center rounded-lg transition duration-500 group-hover:rounded-xl">
             <div class="sm:w-7/12 pl-0 p-5">
               <div class="space-y-2">
                 <div class="space-y-4">
@@ -32,7 +32,7 @@
 
         <div>
           <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
-            <img src="{{asset('img/presenciales/portada-mecanica-de-alto-cilindraje-presencial.png')}}" alt="Mecanica de alto cilindraje" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
+            <img src="{{asset('img/presenciales/portada-mecanica-de-alto-cilindraje-presencial.png')}}" alt="Mecanica de alto cilindraje" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-center rounded-lg transition duration-500 group-hover:rounded-xl">
             <div class="sm:w-7/12 pl-0 p-5">
               <div class="space-y-2">
                 <div class="space-y-4">
@@ -47,7 +47,7 @@
 
         <div>
           <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
-            <img src="{{asset('img/presenciales/portada-mecanica-de-alto-cilindraje-presencial.png')}}" alt="Preparacion electronica alto rendimiento" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
+            <img src="{{asset('img/presenciales/portada-mecanica-de-alto-cilindraje-presencial.png')}}" alt="Preparacion electronica alto rendimiento" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-center rounded-lg transition duration-500 group-hover:rounded-xl">
             <div class="sm:w-7/12 pl-0 p-5">
               <div class="space-y-2">
                 <div class="space-y-4">
@@ -62,7 +62,7 @@
 
         <div>
           <div class="p-1 rounded-xl group sm:flex space-x-6 bg-white bg-opacity-50 shadow-xl hover:rounded-2xl">
-            <img src="{{asset('img/presenciales/portada-mecanica-de-alto-cilindraje-presencial.png')}}" alt="Vehiculos Electricos" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-top rounded-lg transition duration-500 group-hover:rounded-xl">
+            <img src="{{asset('img/presenciales/portada-mecanica-de-alto-cilindraje-presencial.png')}}" alt="Vehiculos Electricos" loading="lazy" width="1000" height="667" class="h-56 sm:h-full w-full sm:w-5/12 object-cover object-center rounded-lg transition duration-500 group-hover:rounded-xl">
             <div class="sm:w-7/12 pl-0 p-5">
               <div class="space-y-2">
                 <div class="space-y-4">
--- resources/views/presenciales/electricidad-basica.blade.php
+++ resources/views/presenciales/electricidad-basica.blade.php
@@ -13,7 +13,7 @@
                 
             </div>
             <div class="hidden lg:mt-0 lg:col-span-5 lg:flex">
-                <img src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/hero/phone-mockup.png" alt="mockup">
+                <img src="{{asset('img/presenciales/electricidad-motocicletas/portada-em.png')}}" alt="Electricidad en Motocicletas">
             </div>                
         </div>
     </section>
