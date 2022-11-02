6ad9e2d845b667c7506a1a289bc937f9d41c5ce8
Santiago Fandiño <santiago.fango@gmail.com>
2022-11-02 15:51:01 -0500

    Correccion diseño 4 tarjetas inyeccion info

--- resources/views/presenciales/inyeccion-electronica.blade.php
+++ resources/views/presenciales/inyeccion-electronica.blade.php
@@ -45,7 +45,7 @@
         </div>
         <section class="pb-10 bg-blueGray-200 -mt-24">
                 <div class="container mx-auto px-4">
-                  <div class="flex flex-wrap">
+                  <div class="flex flex-wrap grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
                     
                     <div class="w-full md:w-4/12 px-4 text-center">
                         <div class="relative flex flex-col min-w-0 break-words bg-white w-full mb-8 shadow-lg rounded-lg">
