5179472ee47efa30c29a394d8a6f7d72e38f4f74
Santiago Fandiño <santiago.fango@gmail.com>
2022-11-08 19:35:43 -0500

    Cambio index cursos, act info

--- resources/views/courses/index.blade.php
+++ resources/views/courses/index.blade.php
@@ -4,7 +4,7 @@
         <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-36 ">
             <div class="w-full md:w-3/4 lg:w-1/2 bg-black bg-opacity-50">
                 <h1 class="text-white font-fold text-6xl">Diversos cursos para Diversas marcas</h1>
-                <p class="text-white text-lg mt-2 mb-2">Encuentra cursos para tu marca favorita y para el uso de las mejores herramientas en inyeccion electronica</p>
+                <p class="text-white text-lg mt-2 mb-2">Encuentra cursos para tu marca favorita y para el uso de las mejores herramientas</p>
                 <!-- buscador -->
                 @livewire('search')
             </div>
