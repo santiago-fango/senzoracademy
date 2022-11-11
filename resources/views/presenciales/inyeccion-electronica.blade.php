0d5c2620495ba18886e90a172a97a6ffde6d5b49
Santiago Fandiño <santiago.fango@gmail.com>
2022-11-11 18:30:41 -0500

    Boton 1, cursos

--- resources/views/presenciales/inyeccion-electronica.blade.php
+++ resources/views/presenciales/inyeccion-electronica.blade.php
@@ -417,6 +417,12 @@
         </div>
     </div>
   </section>
+  <section>
+    <button class="group relative h-12 w-48 overflow-hidden rounded-lg bg-white text-lg shadow">
+      <div class="absolute inset-0 w-3 bg-red-400 transition-all duration-[250ms] ease-out group-hover:w-full"></div>
+      <span class="relative text-black group-hover:text-white">Siguente curso</span>
+    </button>
+  </section>
   
 
 </body>
