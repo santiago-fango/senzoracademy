7b75958b44400d843067d39b1fea7582805bfb21
Santiago Fandiño <santiago.fango@gmail.com>
2022-11-16 13:20:30 -0500

    Correccion ortografica

--- resources/views/presenciales/preparacion-electronica.blade.php
+++ resources/views/presenciales/preparacion-electronica.blade.php
@@ -108,7 +108,7 @@
     <section class="bg-white dark:bg-gray-900">
         <div class="gap-16 items-center py-8 px-4 mx-auto max-w-screen-xl lg:grid lg:grid-cols-2 lg:py-16 lg:px-6">
             <div class="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-                <h3 class="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">Calse I</h2>
+                <h3 class="mb-4 text-4xl font-extrabold text-gray-900 dark:text-white">Clase I</h2>
                 <div class="grid grid-cols-2 gap-4">
                     <ul class="px-2" style="list-style-type: circle;">
                         <li>Principios de la Inyección Electrónica</li>
